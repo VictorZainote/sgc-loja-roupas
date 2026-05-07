9353609a4c021228a64e555eb8763047a4136f4f
VictorZainote <vh.azvd@sempreceub.com>
2026-05-07 20:51:02 -0300

    feat: adiciona serializers da API

--- vendas/models.py
+++ vendas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from produtos.models import Produto
+from clientes.models import Cliente
 
-# Create your models here.
+class Venda(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    valor_total = models.DecimalField(max_digits=10, decimal_places=2)
+    data_venda = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+
+        if self.produto.quantidade_estoque < self.quantidade:
+            raise ValueError("Estoque insuficiente")
+
+        self.valor_total = self.produto.preco * self.quantidade
+
+        self.produto.quantidade_estoque -= self.quantidade
+        self.produto.save()
+
+        super().save(*args, **kwargs)
