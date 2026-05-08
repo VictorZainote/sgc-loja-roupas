3cce71d620bb5a5435926946fdbee56fb1ce8bb8
VictorZainote <vh.azvd@sempreceub.com>
2026-05-07 21:46:44 -0300

    feat: adiciona tratamento de exceções na criação de produtos

--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Produto
 from .serializers import ProdutoSerializer
@@ -8,3 +10,17 @@ class ProdutoViewSet(viewsets.ModelViewSet):
 
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
+
+    def create(self, request, *args, **kwargs):
+
+        try:
+            return super().create(request, *args, **kwargs)
+
+        except Exception as erro:
+
+            return Response(
+                {
+                    'erro': str(erro)
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
