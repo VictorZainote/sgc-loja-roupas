528b9b99699a67206c77674a2cbb67406bcd0bc5
VictorZainote <vh.azvd@sempreceub.com>
2026-05-07 21:20:48 -0300

    feat: aplica migrations e cria tabelas do banco

--- core/urls.py
+++ core/urls.py
@@ -1,27 +1,12 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
-from vendas.views import relatorio_vendas
 
 from produtos.views import ProdutoViewSet
 from clientes.views import ClienteViewSet
 from vendas.views import VendaViewSet
+from vendas.views import relatorio_vendas
 
 router = DefaultRouter()
 
@@ -31,6 +16,11 @@ router.register(r'vendas', VendaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include(router.urls)),
-    path('relatorios/vendas/', relatorio_vendas),
-] 
+
+    path(
+        'relatorios/vendas/',
+        relatorio_vendas
+    ),
+]
--- vendas/views.py
+++ vendas/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Venda
 from .serializers import VendaSerializer
+
 
 class VendaViewSet(viewsets.ModelViewSet):
 
