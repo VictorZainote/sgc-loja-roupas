086ab74f9abad9a10cf192d36cda938c376ad806
VictorZainote <vh.azvd@sempreceub.com>
2026-05-17 15:43:55 -0300

    feat: adiciona integração com API ViaCEP

---
+++ clientes/services/viacep.py
@@ -0,0 +1,30 @@
+import requests
+
+
+def buscar_cep(cep):
+
+    cep = cep.replace("-", "").strip()
+
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+
+    try:
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            return None
+
+        dados = response.json()
+
+        if "erro" in dados:
+            return None
+
+        return {
+            "rua": dados.get("logradouro"),
+            "bairro": dados.get("bairro"),
+            "cidade": dados.get("localidade"),
+            "estado": dados.get("uf")
+        }
+
+    except:
+        return None
