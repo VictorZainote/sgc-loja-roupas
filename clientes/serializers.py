2090289b8679ef0f7cde927da1ec72f8e041b421
VictorZainote <vh.azvd@sempreceub.com>
2026-06-17 12:35:57 -0300

    feat: validação de email e cpf

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
+from vendas import models
 from .models import Cliente
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+        email = models.EmailField(unique=True)
+    def validate_email(self, value):
+        if Cliente.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "Email já cadastrado"
+            )
+        return value
+    def validate_cpf(self, value):
+        if Cliente.objects.filter(cpf=value).exists():
+            raise serializers.ValidationError(
+                "CPF já cadastrado"
+            )
+        return value
