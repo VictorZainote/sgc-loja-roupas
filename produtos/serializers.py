458d14381dda9be9bc402ebd5d2fcb6fe173df49
VictorZainote <vh.azvd@sempreceub.com>
2026-06-17 12:38:49 -0300

    feat: validação nos preços e na quantidade do estoque

--- produtos/serializers.py
+++ produtos/serializers.py
@@ -5,3 +5,17 @@ class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
         fields = '__all__'
+
+    def validate_preco(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                "Preço deve ser maior que zero"
+            )
+        return value
+
+    def validate_quantidade_estoque(self, value):
+        if value < 0:
+            raise serializers.ValidationError(
+                "Estoque não pode ser negativo"
+            )
+        return value        
